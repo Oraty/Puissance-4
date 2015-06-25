f372665e3a0412e9d4f490590956baa40a781e1d
Loucas Cubeddu <ytaroperso@gmail.com>
2015-06-25 19:53:22 +0200

    maj method play

--- Puissance4.cpp
+++ Puissance4.cpp
@@ -2,6 +2,23 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
+#include <typeinfo>
+
+int getInt(){
+  double num;
+
+  std::cin >> num;  // This needs validation too.
+  std::cin.ignore(80, '\n');
+  while (num != static_cast<int>(num))
+  {
+    std::cout << "Désolé, ce n'est pas un nombre: ";
+    std::cin >> num;
+    std::cin.ignore(80, '\n');
+  }
+  std::cin.ignore(80, '\n');
+  return static_cast<int>(num);
+}
+
 PuissanceQuatre::PuissanceQuatre(){
     for (auto& ligne : grille){  //for every line of "grille"
         for (auto& kase : ligne){// for every case of every line of "grille" //kase = case
@@ -11,6 +28,9 @@ PuissanceQuatre::PuissanceQuatre(){
 }
 
 void PuissanceQuatre::print(){ // print 'O' for yellow and 'X' for red
+    for (unsigned i{0};i<500;i++)
+        std::cout << std::endl;
+
     for (auto ligne : grille){     //for every line of "grille"
         std::cout << " |";
         for (auto kase : ligne){  // for every case of every line of "grille" //kase = case
@@ -40,46 +60,76 @@ std::cout << '=' << '=';
  *si le coup est invalide le faire jouer*
  */
 
- bool PuissanceQuatre::play(){
-    std::cout << "Quelle est la couleur du premier joueur? (jaune ou rouge)" << std::endl;
-    std::string joueur;
-    std::cin >> joueur;
-    try{
-        if (joueur != "jaune" && joueur != "rouge"){
-            throw std::invalid_argument("L'information rentrée n'est ni jaune ni rouge \n");
+Couleur PuissanceQuatre::askPlayerColor(){
+    bool isJauneOrRouge;
+    Couleur jeton;
+    do{
+        isJauneOrRouge = false;
+        std::cout << "Quelle est la couleur du premier joueur? (jaune ou rouge)" << std::endl;
+        std::string joueur;
+        std::cin >> joueur;
+        try{
+            if (joueur != "jaune" && joueur != "rouge"){
+                throw std::invalid_argument("L'information rentrée n'est ni jaune ni rouge \n");
+            }else if (joueur == "jaune")jeton = jaune;
+            else jeton = rouge;
         }
-    }
-    catch (std::exception const& invArg){
-    std::cout << invArg.what() ;
-    return 0;
-    }
-    //ICI
-    std::cout << "Sur quelle colonne jouer? (1-7)" << std::endl;
+        catch (std::exception const& invArg){
+        std::cout << invArg.what() ;
+        isJauneOrRouge = true;
+        }
+    }while(isJauneOrRouge);
+    return jeton;
+}
+
+unsigned int PuissanceQuatre::askPlayerColumn(){
+    bool isArgValid;
     unsigned int colonne{0};
-    std::cin >> colonne;
-    try{
-        if (colonne < 0 || colonne > 7){
-            throw std::invalid_argument("L'information rentrée n'est pas comprise entre 1 et 7 \n");
+    do{
+        isArgValid = false;
+        std::cout << "Sur quelle colonne jouer? (1-7)" << std::endl;
+        colonne = getInt();
+        try{
+            if (colonne <=0 || colonne > 7){
+                throw std::invalid_argument("L'information rentrée n'est pas comprise entre 1 et 7 \n");
+            }
         }
-    }
-    catch (std::exception const& invArg){
-    std::cout << invArg.what() ;
-    return 0;
-    }
+        catch (std::exception const& invArg){
+            std::cout << invArg.what() ;
+            isArgValid = true;
+        }
+        --colonne; // On fait correspondre le nombre entré avec l'index du tableau (0-6)
+    }while(isArgValid);
+    return colonne;
+}
+
+
+ void PuissanceQuatre::play(){
+    auto jeton = PuissanceQuatre::askPlayerColor();
+    decltype(jeton)& joueur{jeton};
+    bool win;
+    do{
+    auto colonne = PuissanceQuatre::askPlayerColumn();
+    int ligne{5};
     try{
-        for (size_t i{0};grille[i][colonne] !=vide;++i){
-            if (i>=7){
-                throw std::out_of_range("La colonne est pleine!");
+        while (grille[ligne][colonne] !=vide){// on parcourt la colonne jusqu'a trouver une case vide
+            if (ligne<=0){// Si la colonne est pleine, erreur.
+                throw std::out_of_range("La colonne est pleine! \n");
+            }
+            else{
+            --ligne;
             }
         }
     }
     catch(std::exception const& __arg){
         std::cout << __arg.what();
-        //Comment faire ici pour aller au point marqué "ICI" par un commentaire sans utiliser de goto?
     }
-
-
-return 1;
+    grille[ligne][colonne] = jeton;
+    if (joueur != rouge)joueur = rouge;
+    if (joueur != jaune)joueur = jaune;
+    win = false;
+    this->print();
+    }while(!win);
 }
 
 
--- Puissance4.h
+++ Puissance4.h
@@ -2,16 +2,26 @@
 #define PUISSANCE4_H_INCLUDED
 #include <array>
 
-enum Couleur {vide ,jaune ,rouge};
+enum Couleur {vide, jaune, rouge};
 using Grille = std::array<std::array<Couleur,7>,6>;
 class PuissanceQuatre{
     private:
     Grille grille;
-    Couleur jeton;
+    Couleur askPlayerColor();
+    unsigned int askPlayerColumn();
     public:
     PuissanceQuatre();
     void print();
-    bool play();
+    void play();
 };
 
 #endif // PUISSANCE4_H_INCLUDED
+
+
+
+
+
+
+
+
+
